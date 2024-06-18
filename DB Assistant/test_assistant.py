8ef5d07244b92c8da6a8d78b37620ff14fee4ac3
oscardenaslu <oscardenas@somos.likeu.co>
2024-06-17 21:14:41 -0500

    Successful db connection

--- DB Assistant/test_assistant.py
+++ DB Assistant/test_assistant.py
@@ -3,11 +3,13 @@ import openai
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
-# import passwd
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
 
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("API_KEY")
 
 # Set API key as environment variable
 os.environ['OPENAI_API_KEY'] = api_key
@@ -15,11 +17,11 @@ os.environ['OPENAI_API_KEY'] = api_key
 client = OpenAI()
 
 # Funcion para transformar lenguaje natural a sintaxis SQL
-def natlang_to_sql(pregunta, table_str):
+def promt_to_sql(pregunta, table_str):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"Eres un asistente que convierte preguntas en consultas SQL, solamente retornaras la sintaxis SQL correspondiente a la consulta. {table_str}"},
+            {"role": "system", "content": f"You're and AI assistant that will help me create SQL queries based on the following table schema: {table_str}"},
             {"role": "user", "content": pregunta}
         ]
     )
@@ -27,13 +29,32 @@ def natlang_to_sql(pregunta, table_str):
     sql_query = response.choices[0].message.content.strip()
     return sql_query
 
-# Ejemplo con tabla lk_pedidos_cstm
-table_str = """
-La tabla lk_pedidos_cstm tiene las siguientes columnas: id_c, ciclo_c, valor_venta_sin_iva_c. El campo ciclo_c es un VARCHAR con estructura año-mes, por ejemplo, 202401 
-donde 2024 representa el año y el 01 representa el ciclo.
-"""
+def conn_db():
+    conn_str = os.getenv("CONNECTION_STRING")
+    # Create a connection to the database
+    engine = create_engine(conn_str)
+    return engine
 
-pregunta = "Necesito todos los pedidos del ciclo 3"
-natlang_to_sql(pregunta, table_str)
+def execute_query(query):
+    # Connect to the database
+    engine = conn_db()
+    df = pd.read_sql(query, engine)
+    return df
 
-print()
+def table_schema(table_name):
+    # Connect to the database
+    engine = conn_db()
+    # Get the schema of the database
+    query_sche = f"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
+
+    df = pd.read_sql(query_sche, engine)
+    table_schema = df.to_string(index=False, header=False)
+    table_sch = "Table Schema: " + table_schema
+    return table_sch
+
+
+table_sch = table_schema("lk_pedidos_cstm")
+pregunta = "I need to know the total amount of orders that were placed in the last 30 days."
+query = promt_to_sql(pregunta, table_sch)
+
+result = execute_query(query)
