b0718cc8a3711cc2c76ac276a99fe4ad7642758e
Brandon PG <brperez@somos.likeu.co>
2024-07-30 08:22:26 -0500

    First version SQL assistant

--- Asistente SQL/sql_assistant.py
+++ Asistente SQL/sql_assistant.py
@@ -1,10 +1,14 @@
-# pip install langchain openai sqlalchemy python-dotenv langchain-openai langchain-experimental pymysql python-dotenv langchain-community tiktoken
+# pip install langchain openai sqlalchemy python-dotenv langchain-openai langchain-experimental pymysql python-dotenv langchain-community tiktoken jq
 
+import json
 import os
+import csv
+import ast
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import TokenTextSplitter
 from sqlalchemy import create_engine
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI, OpenAI
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.chains import create_sql_query_chain
@@ -14,7 +18,11 @@ import tiktoken
 from langchain_community.vectorstores import Chroma
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.agent_toolkits import create_sql_agent
+from langchain.schema import Document
+from langchain_community.callbacks.manager import get_openai_callback
+from pprint import pprint
+from langchain_community.document_loaders import JSONLoader
+
 
 load_dotenv()
 
@@ -31,103 +39,126 @@ def conn_db():
     return engine
 
 db_engine = conn_db()
+
 ##call cstm, calls, leads_cstm
 db = SQLDatabase(engine=db_engine, include_tables=["leads", "lk_pedidos_cstm", "leads_lk_pedidos_1_c"])
+# db = SQLDatabase(engine=db_engine)
+llm1 = ChatOpenAI(temperature=0, verbose=True)
 llm = OpenAI(temperature=0, verbose=True)
-db_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=True, return_direct=True, return_sql=False)
+query_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=False, return_direct=True, use_query_checker=False, return_sql=False)
 
-##Examples
-examples = [
-    {"input": "Dame el Id de Lucy Yohana Cespedes Ramos", "query": "SELECT id FROM leads WHERE leads.last_name='Lucy Yohana Cespedes Ramos';"},
+# Traeme el Nombre , cedula , usuario o liker lo tiene asignado, ciclo y valor del pedido de los leads o novaempresarios para el ciclo 202408, No limites la respuesta
+human_input = "Traeme el Nombre , cedula , usuario o liker lo tiene asignado, ciclo y valor del pedido de los leads o novaempresarios para el ciclo 202408, No limites la respuesta"
+########################################################       
+# Define el esquema JSON para extraer tablas y campos
+# json_schema = {
+#     "title": "Table",
+#     "description": "Tables and fields in SQL database.",
+#     "type": "object", ##Error a
+#     "properties": {
+#         "table_name": {
+#             "type": "string",
+#             "description": "Name of the table in the database.",
+#         },
+#         "fields": {
+#             "type": "array",
+#             "items": {
+#                 "type": "string",
+#                 "description": "Field names in the table.",
+#             },
+#         },
+#     },
+#     "required": ["table_name", "fields"],
+# }
+########################################################
+# Convertir JSON a documentos
+loader = JSONLoader(
+    file_path='utils\contexto_tablas.json',
+    # jq_schema='.modulos | to_entries | map({table_name: .key, fields: (.value.campos | keys)})',
+    # is_content_key_jq_parsable=True,
+    jq_schema='.modulos',
+    text_content=False)
+
+table_names = loader.load()
+pprint(table_names)
+
+tables_model_prefix = """Devuelve solo los nombres de la tablas SQL y los campos que SON relevantes para la pregunta del usuario, especifica muy bien a que tabla correponde cada campo.
+No debes inventar datos, solo elegir con repecto al mensaje human cuales son las tablas donde se podira encontrar esta data basado en el siguiente JSON:
+
+{tables_names}
+
+No limites tu respuesta"""
+
+prompt_tables = PromptTemplate(
+    template=tables_model_prefix,
+    input_variables=["input" ,"tables_names"]
+)
+
+full_prompt_tables = ChatPromptTemplate.from_messages(
+    [
+        SystemMessagePromptTemplate(prompt=prompt_tables),
+        ("human", "{input}"),
+        MessagesPlaceholder("agent_scratchpad"),
+    ]
+)
+
+# structured_llm = llm1.with_structured_output(json_schema)
+
+prompt_val_tables = full_prompt_tables.invoke(
     {
-        "input": "cual es el nombre de quien llamo a Lucy Yohana Cespedes Ramos, su estatus y en que fechas",
-        "query": "SELECT leads.last_name, users.user_name, calls.date_entered, calls.created_by, calls.status FROM leads JOIN calls on calls.parent_id=leads.id JOIN users on users.id=calls.created_by WHERE leads.id='10002d2b-c3db-6e82-1fd9-64dcfb3d016d';",
-    },
-    {
-        "input": "Total de pedidos realizados por Lucy Yohana Cespedes Ramos",
-        "query": "SELECT COUNT(lk_pedidos.id) as total_pedidos FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos';",
-    },
-    {
-        "input": "Detalles de los pedidos con valor superior a 1000 sin IVA de Lucy Yohana Cespedes Ramos",
-        "query": "SELECT lk_pedidos.id, lk_pedidos.name, lk_pedidos_cstm.valor_venta_sin_iva_c FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb JOIN lk_pedidos_cstm ON lk_pedidos_cstm.id_c = lk_pedidos.id WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos' AND lk_pedidos_cstm.valor_venta_sin_iva_c > 1000;",
-    },
-    {
-        "input": "Fecha del último pedido de Lucy Yohana Cespedes Ramos",
-        "query": "SELECT MAX(lk_pedidos.date_entered) as ultimo_pedido FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos';",
-    },
-    {
-        "input": "¿Quién es el vendedor asignado a Lucy Yohana Cespedes Ramos?",
-        "query": "SELECT users.user_name FROM leads JOIN users ON leads.assigned_user_id = users.id WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos';",
-    },
-    {
-        "input": "¿Cuál es el presupuesto total gastado por Lucy Yohana Cespedes Ramos en todos los ciclos?",
-        "query": "SELECT SUM(lk_pedidos_cstm.presupuesto_c) as total_presupuesto FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb JOIN lk_pedidos_cstm ON lk_pedidos_cstm.id_c = lk_pedidos.id WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos';",
-    },
-    {
-        "input": "Dame los pedidos y presupuesto por ciclo de Lucy Yohana Cespedes Ramos",
-        "query": "SELECT lk_pedidos_cstm.ciclo_c, lk_pedidos.name, lk_pedidos_cstm.presupuesto_c FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb JOIN lk_pedidos_cstm ON lk_pedidos_cstm.id_c = lk_pedidos.id WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos';",
-    },
-    {
-        "input": "¿Cuántos pedidos realizó Lucy Yohana Cespedes Ramos en el ciclo 202311?",
-        "query": "SELECT lk_pedidos_cstm.pedidos_realizados_c FROM leads JOIN leads_lk_pedidos_1_c ON leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida = leads.id JOIN lk_pedidos ON lk_pedidos.id = leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb JOIN lk_pedidos_cstm ON lk_pedidos_cstm.id_c = lk_pedidos.id WHERE leads.last_name = 'Lucy Yohana Cespedes Ramos' AND lk_pedidos_cstm.ciclo_c = 202311;",
-    },
-    {
-        "input": "Dame los clientes potenciales del departamento de Antioquia",
-        "query": "SELECT leads.last_name, leads.phone_mobile FROM leads WHERE leads.department = 'Antioquia';",
-    },
-    {
-        "input": "Dame los pedidos realizados por Lucy Yohana Cespedes Ramos en el ciclo 202311",
-        "query": 'SELECT COUNT(*) FROM "Employee"',
-    },
-    {
-        "input": "Dame todos los vendedores y sus clientes asignados",
-        "query": 'SELECT users.user_name, leads.last_name FROM leads JOIN users ON leads.assigned_user_id = users.id;',
-    },
-    {
-        "input": "Dame el total de pedidos por ciclo en el año 2024",
-        "query": 'SELECT lk_pedidos_cstm.ciclo_c, COUNT(lk_pedidos.id) as total_pedidos FROM lk_pedidos JOIN lk_pedidos_cstm ON lk_pedidos_cstm.id_c = lk_pedidos.id WHERE lk_pedidos.name like "2024%" GROUP BY lk_pedidos_cstm.ciclo_c',
-    },
-    #Aqui estan las querys de likeU
-    {
-        "input": "Pedidos Vs Cilos de Lucy Yohana Cespedes Ramos",
-        "query": 'SELECT last_name, lk_pedidos.name FROM leads JOIN leads_lk_pedidos_1_c on leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida=leads.id JOIN lk_pedidos on lk_pedidos.id=leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb WHERE leads.id = "10002d2b-c3db-6e82-1fd9-64dcfb3d016d" AND lk_pedidos.name!="" ORDER BY "lk_pedidos","name" ASC;',
-    },
-    {
-        "input": "Cual es el valor sin iva de Lucy Yohana Cespedes Ramos en todos los periodos",
-        "query": 'SELECT last_name, lk_pedidos.name, lk_pedidos_cstm.valor_venta_sin_iva_c FROM leads JOIN leads_lk_pedidos_1_c on leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida=leads.id JOIN lk_pedidos on lk_pedidos.id=leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb JOIN lk_pedidos_cstm on lk_pedidos_cstm.id_c=lk_pedidos.id WHERE leads.id = "10002d2b-c3db-6e82-1fd9-64dcfb3d016d" AND lk_pedidos.name!="" ORDER BY "lk_pedidos","name" ASC;',
-    },
-    ##important
-     {
-         "input": "Traeme el Nombre , identificacion o cedula , usuario o liker lo tiene asignado, ciclo y valor del pedido de los leads o novaempresarios para el ciclo 202409",
-         "query": 'SELECT leads.last_name , leads_cstm.identificacion_c ,users.user_name,lk_pedidos.name, lk_pedidos_cstm.valor_venta_sin_iva_c FROM leads  JOIN leads_cstm on leads_cstm.id_c=leads.id join users on users.id=leads.assigned_user_id JOIN leads_lk_pedidos_1_c on leads_lk_pedidos_1_c.leads_lk_pedidos_1leads_ida=leads.id  JOIN lk_pedidos on lk_pedidos.id=leads_lk_pedidos_1_c.leads_lk_pedidos_1lk_pedidos_idb  JOIN lk_pedidos_cstm on lk_pedidos_cstm.id_c=lk_pedidos.id  WHERE lk_pedidos.name="202409";',
-        
+        "input": human_input,
+        "tables_names": table_names,
+        "agent_scratchpad": [],
     }
-]
+)
 
-###Vectore store con FAISS
+# print(prompt_val_tables.to_string())
+
+# with get_openai_callback() as cb:
+#     tables_response = llm.invoke(prompt_val_tables)
+#     print(f"Total tokens used: {cb.total_tokens}")
+#     print(f"Prompt tokens used: {cb.prompt_tokens}")
+#     print(f"Completion tokens used: {cb.completion_tokens}")
+#     print(f"Total cost: ${cb.total_cost:.5f}")
+
+# tables_response = structured_llm.invoke(prompt_val_tables)
+tables_response = llm.invoke(prompt_val_tables)
+print(tables_response)
+
+
+##########################################################
+###COntext automatico de la BD
+# context = db.get_context()
+# print(list(context))
+# print(context["table_names"])
+
+##############################################################
+###Vectore store con FAISS para los ejemplos
+with open('utils\query_examples.json', 'r') as file:
+    query_examples = json.load(file)
+
 example_selector = SemanticSimilarityExampleSelector.from_examples(
-    examples,
+    query_examples,
     OpenAIEmbeddings(),
     FAISS,
     k=2,
     input_keys=["input"],
 )
-
+ ########################################################       
 # Define a simple example prompt
-
-prefix = """Eres un experto en MySQL. Dada una pregunta de entrada, crea una consulta MySQL sintácticamente correcta para ejecutar. 
+sql_model_prefix = """Eres un experto en {dialect}. Dada una pregunta de entrada, crea una consulta {dialect} sintácticamente correcta para ejecutar. 
 A menos que se especifique lo contrario, no devuelvas más de {top_k} filas.
+Aquí está la información relevante de las tabla: {table_info}
 A continuación, se presentan varios ejemplos de preguntas y sus consultas SQL correspondientes:\n\n"""
 
 example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
+
 prompt = FewShotPromptTemplate(
     example_selector=example_selector,
-    example_prompt=PromptTemplate.from_template(
-        "User input: {input}\nSQL query: {query}"
-    ),
-    prefix=prefix,
+    example_prompt=example_prompt,
+    prefix=sql_model_prefix,
     suffix="",
-    input_variables=["input", "dialect", "top_k"],
+    input_variables=["input", "dialect", "top_k", "table_info"],
 )
 
 full_prompt = ChatPromptTemplate.from_messages(
@@ -140,90 +171,124 @@ full_prompt = ChatPromptTemplate.from_messages(
 
 prompt_val = full_prompt.invoke(
     {
-        "input": "Traeme el Nombre , cedula , usuario o liker lo tiene asignado, ciclo y valor del pedido de los leads o novaempresarios para el ciclo 202408, no limites la respuesta",
+        "input": human_input,
         "top_k": 5,
-        "dialect": "MySQL",
+        "dialect": "MariaDB",
+        "table_info": tables_response,
         "agent_scratchpad": [],
     }
 )
-# print(prompt_val.to_string())
+print(prompt_val.to_string())
 
 #############################################
 ##Crear agente SQL
-# print('Hola mundo')
-# agent = create_sql_agent(
-#     llm=llm,
-#     db=db,
-#     prompt=full_prompt,
-#     verbose=True,
-#     agent_type="openai-tools",
-# )
-
-
-# Test the agent
-# response = agent.invoke({
-#     "input": "Traeme el Nombre , identificacion o cedula , usuario o liker lo tiene asignado, ciclo y valor del pedido de los leads o novaempresarios para el ciclo 202409, no limites la respuesta",
-#     "top_k": 5,
-#     "dialect": "MySQL",
-#     "agent_scratchpad": [],
-# })
-##################################################
-
-# print(formatted_prompt)
-# Contar los tokens del prompt formateado
-#Modelos disponibles ['gpt2', 'r50k_base', 'p50k_base', 'p50k_edit', 'cl100k_base', 'o200k_base']
-# enc = tiktoken.get_encoding("gpt2")
-# num_tokens = len(enc.encode(formatted_prompt))
-
-# TOKEN_LIMIT = 5000
-# return_direct = num_tokens > TOKEN_LIMIT
 
 ####################################################
-###Answer to CSV
+# Function to save LLM response to CSV
+def save_response_to_csv(response, filename='llm_response.csv'):
+    # Verificar si la respuesta es una cadena y convertirla a una lista de tuplas
+    if isinstance(response, str):
+        response = ast.literal_eval(response)
 
-def save_to_file(data, filename):
-    with open(filename, 'w') as file:
-        for item in data:
-            file.write(f"{item}")
+    # Verificar el número máximo de columnas en la respuesta
+    max_columns = max(len(row) for row in response)
+    headers = [f'Column{i+1}' for i in range(max_columns)]
 
-####################################################
-query_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=True, return_direct=True, return_sql=False)
+    with open(filename, 'w', newline='') as output_file:
+        csv_writer = csv.writer(output_file)
+        # Escribir la fila de encabezado
+        csv_writer.writerow(headers)
+        # Escribir los datos
+        csv_writer.writerows(response)
 
-
-
-# print(prompt_val.to_string())
+########################################################
+###Primera cadena, Cosulta a la SQL
 sql_query = query_chain.invoke(prompt_val)
-
-print(sql_query["result"])
-print(sql_query)
+# print(sql_query["result"])
 
 
 
+##########################################################
+#Callback para tokens del input, tokens del output, total de tokens y costo de los tokens
+with get_openai_callback() as cb:
+    sql_query = query_chain.invoke(prompt_val)
+    print(f"Total tokens used: {cb.total_tokens}")
+    print(f"Prompt tokens used: {cb.prompt_tokens}")
+    print(f"Completion tokens used: {cb.completion_tokens}")
+    print(f"Total cost: ${cb.total_cost:.5f}")
+
+
+print("FIN DE LA PRIMERA CADENA")
+
+########################################################
+#Segunda Cadena, Pasar o no el contexto al LLM
+
+llm_model_prefix = """Eres parte de una cadena de LangChain y recibirás una respuesta de una consulta a una base de datos SQL en el siguiente formato: [(columna1, columna2, ...), (columna1, columna2, ...), ...]. 
+Quiero que proceses esta información y respondas a la pregunta del de la mejor manera posible, sin rebundar en detalles, centrate en dar un respuesta lo mas ligera posible pero sin inventar ningun dato, utilizando únicamente la información disponible en la respuesta sin inventar datos, la pregunta es: {input} 
+Aquí está la respuesta de la consulta: {response_query}\n\n"""
+
+prompt_llm = PromptTemplate(
+    template=llm_model_prefix,
+    input_variables=["response_query"]
+)
+
+full_prompt_llm = ChatPromptTemplate.from_messages(
+    [
+        SystemMessagePromptTemplate(prompt=prompt_llm),
+        MessagesPlaceholder("agent_scratchpad"),
+    ]
+)
+
+prompt_val_llm = full_prompt_llm.invoke(
+    {
+        "input": human_input,
+        "response_query": sql_query["result"],
+        "agent_scratchpad": [],
+    }
+)
+
+####################################################
+####Funcion para contar Tokens
+total_tokens_used = 0
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+tokens = encoding.encode(prompt_val_llm.to_string())
+total_tokens = len(tokens)
+
+# with get_openai_callback() as cb:
+#     llm_response = llm.invoke(prompt_val_llm)
+#     print(f"Total tokens used: {cb.total_tokens}")
+#     print(f"Prompt tokens used: {cb.prompt_tokens}")
+#     print(f"Completion tokens used: {cb.completion_tokens}")
+#     print(f"Total cost: ${cb.total_cost:.5f}")
+
+# print(f"Total tokens in sql_query['result']: {total_tokens}")
+####################################################
+###Validacion de tokens y respuesta
+try:
+    llm_response = llm.invoke(prompt_val_llm)
+    print(llm_response)
+except Exception as e:
+    # En caso de error, guarda la respuesta en un CSV y muestra el resultado
+    save_response_to_csv(sql_query["result"])
+    print(sql_query["result"])
+#########################################################
+##Respuestas, tokens y metadata
+# sql_query = query_chain.invoke(prompt_val)
+
+# save_response_to_csv(sql_query["result"])
+# print(type(sql_query["result"]))
+
+# with get_openai_callback() as cb:
+#     sql_query = query_chain.invoke(prompt_val)
+#     print(f"Total tokens used: {cb.total_tokens}")
+#     print(f"Prompt tokens used: {cb.prompt_tokens}")
+#     print(f"Completion tokens used: {cb.completion_tokens}")
+#     print(f"Total cost: ${cb.total_cost:.5f}")
 
 
 
-
-## Definicion de tablas
-# context = db.get_context()
-
-# prompt_with_context = chain.get_prompts()[0].partial(table_info=context["table_info"])
-# print(prompt_with_context.pretty_repr()[:1500])
-
-#######Hacer la QA para SQL
-##AAA
-
-###
-
-
-
-# print(db.get_usable_table_names())
-# print(SQLDatabase.get_table_info(["leads_lk_pedidos_1_c"]))
-# result = db_chain.invoke("Traeme todos los registros de la tabla leads")
-
-
-# result = chain.invoke({"question": "Dame el saldo actual del primer registro de la tabla leads"})
-##Funciones con python por promp
-# save_to_file(result['result'], 'combined_results.txt')
-# print(result)
-
-# print(result['result'])
+##Estructura de la respuesta 
+# sql_query["result"] 
+# sql_query["query"]
